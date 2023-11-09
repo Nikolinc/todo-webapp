8e12877d3e659814d0b12169c05599d27b8a154a
Nikolinc <tsigulskynikita@gmail.com>
2023-11-10 01:50:34 +0500

    feat:add store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './global.css'
+import router from './router'
+import { RouterProvider } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
